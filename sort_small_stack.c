89cf8849cadd634eaf35d491e8314cc154ee946e
Longfellow Atahbah <latahbah@at-q5.msk.21-school.ru>
2022-03-02 14:10:57 +0300

    fixed small sort stack func()

--- sort_small_stack.c
+++ sort_small_stack.c
@@ -6,7 +6,7 @@
 /*   By: latahbah <latahbah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/13 19:10:26 by latahbah          #+#    #+#             */
-/*   Updated: 2022/02/14 23:05:32 by latahbah         ###   ########.fr       */
+/*   Updated: 2022/02/25 20:56:42 by latahbah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,28 +53,34 @@ static void	sort_four_nums(t_data *data)
 			pb(data);
 		ops_needed--;
 	}
-	sort_three_nums(data, 1, 2);
+	if (data->a.stack[0] > data->a.stack[1]
+		|| data->a.stack[1] > data->a.stack[2]
+		|| data->a.stack[0] > data->a.stack[2])
+		sort_three_nums(data, 1, 2);
 	pa(data);
 }
 
 static void	sort_five_nums(t_data *data)
 {
 	int	i;
-	int	ops_needed;
+	int	kk;
 
 	i = 0;
-	ops_needed = 5;
-	while (ops_needed)
+	kk = 5;
+	while (kk)
 	{
 		if (data->a.stack[0] != 0 && data->a.stack[0] != 1)
 			ra(data);
 		else
 			pb(data);
-		ops_needed--;
+		kk--;
 	}
 	if (data->b.stack[0] < data->b.stack[1])
 		sb(data);
-	sort_three_nums(data, 2, 3);
+	if (data->a.stack[0] > data->a.stack[1]
+		|| data->a.stack[1] > data->a.stack[2]
+		|| data->a.stack[0] > data->a.stack[2])
+		sort_three_nums(data, 2, 3);
 	pa(data);
 	pa(data);
 }
@@ -91,5 +97,7 @@ void	sort_small_stack(t_data *data)
 	else if (size == 4)
 		sort_four_nums(data);
 	else
+	{
 		sort_five_nums(data);
+	}
 }
